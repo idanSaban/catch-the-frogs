0337e169a8812cdd72a9ee0c2cfdfe6ab64eac56
Idan Saban <idan23s.g@gmail.com>
2019-02-11 10:30:50 +0200

    change font

--- dist/render.js
+++ dist/render.js
@@ -1,8 +1,10 @@
 const Renderer = () => {
     const renderFrogs = (frogs) => {
         $("#game").empty();
-        if (frogs) {
-            for (frog of frogs) {
+        if (frogs)
+        {
+            for (frog of frogs)
+            {
                 let output = `<i data-id="${frog.id}" class="fas fa-frog" style="position:absolute;left:${frog.position.x}%;top:${frog.position.y}%;color:${frog.color};font-size:${frog.size}px" ></i>`;
                 $("#game").append(output);
             }
@@ -10,13 +12,13 @@ const Renderer = () => {
 
         $("#level").text(`LEVEL ${game.getLevel()}`);
         $("#frogsLeft").text(`${game.getFrogsLeft()} FROGS LEFT!`);
-        $("#start").text("start");
+        $("#start").text("START");
     }
 
     const gameOver = () => {
         $("#game").empty();
         $("#header").empty();
-        $("#start").text("try again!");
+        $("#start").text("TRY AGAIN!");
     }
 
     return {
